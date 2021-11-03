e4916356db354e90ca99050accbea0deaeeb39b2
Matan Omer <mattanomer@gmail.com>
2021-11-03 16:33:14 +0200

    added handle new loc

--- js/app.controller.js
+++ js/app.controller.js
@@ -13,6 +13,7 @@ function onInit() {
             console.log('Map is ready');
         })
         .catch(() => console.log('Error: cannot init map'));
+        locService.renderLocs();
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,12 +1,43 @@
 export const locService = {
-    getLocs
+    getLocs, renderLocs
 }
 
-
+//{id, name, lat, lng, weather, createdAt, updatedAt}
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+    {id: 1, name: 'Haifa', lat: 32.788011, lng: 34.832581, weather: '30', createdAt: '1', updatedAT: 'dh'}, 
 ]
+
+function handleNewLoc(name,lat,lng,weather,createAt,updatedAT){
+    const newLocId = locs.length
+    var newLocData = {
+        id: newLocId,
+        name,
+        lat,
+        lng,
+        weather,
+        createAt,
+        updatedAT
+    };
+    locs.push(newLocData)
+}
+
+function SaveToLocalStorage(name, loc){
+    localStorage.setItem(name.JSON.stringify(loc)) 
+}
+
+function LoadFromStorage(name){
+    return JSON.parse(localStorage.getItem(name));
+}
+
+function renderLocs(){
+    console.log(locs);
+    var strHtml
+    locs.map(loc => {
+        strHtml += `<p="onclick="setSelectedLoc('${loc.id}')">${loc.name}, weather is good</p>`
+    })
+    document.querySelector('.location-container').innerHTML = strHtml;
+}
+
 
 function getLocs() {
     return new Promise((resolve, reject) => {
